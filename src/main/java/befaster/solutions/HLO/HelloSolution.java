49fd187ccbcd995f7314530c1c55d52d17c4344f
sbx_user1061 <sbx_user1061@ip-10-129-217-27.eu-west-2.compute.internal>
2018-08-19 20:48:14 +0000

    Sun Aug 19 20:48:14 UTC 2018

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -4,17 +4,8 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
-    	String helloString=null;
-    	if(friendName!=null&&!friendName.isEmpty()){
-    		helloString="Hello"+friendName;	
-    	return helloString;
     	
-    	}else if(friendName.isEmpty()){
-    		helloString="Hello World";
-        return helloString;
-    	}     
-    	else{
-        throw new SolutionNotImplementedException();
+    	return "Hello World";
     }
 }
-}
+
